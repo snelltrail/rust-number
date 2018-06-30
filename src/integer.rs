66548970c070571044e7f1230cc0a7e2a2b910d6
Joel Laity <joel.laity@gmail.com>
2018-06-30 14:06:44 +1200

    Implement subtraction

--- src/integer.rs
+++ src/integer.rs
@@ -2,6 +2,8 @@ use std::cmp::max;
 use std::cmp::Ordering;
 use std::ops::AddAssign;
 use std::ops::Add;
+use std::ops::Sub;
+use std::ops::SubAssign;
 
 #[derive(Debug, Eq, PartialEq)]
 
@@ -49,6 +51,42 @@ impl Int {
             i += 1;
         }
     }
+
+    fn remove_leading_zeros(&mut self) {
+        while self.digits.len() > 1 && *self.digits.last().unwrap() == 0u32 {
+            self.digits.pop();
+        }
+    }
+
+    fn borrow_from_neighbour(&mut self, neighbour: usize) {
+        assert!(neighbour < self.digits.len());
+        let mut curr = neighbour;
+        while self.digits[curr] == 0 {
+            self.digits[curr] = u32::max_value();
+            curr += 1;
+            assert!(curr < self.digits.len());
+        }
+        self.digits[curr] -= 1;
+    }
+
+    fn subtract_ignoring_sign(&mut self, rhs: &Int) {
+        assert!(match less_in_magnitude(self, rhs){
+            Ordering::Less => false,
+            Ordering::Equal => true,
+            Ordering::Greater => true,
+        });
+        for i in 0..self.digits.len() {
+            let curr_rhs_digit = match rhs.digits.get(i) {
+                Some(x) => x,
+                None => &0u32,
+            };
+           if self.digits[i] < *curr_rhs_digit {
+            self.borrow_from_neighbour(i+1);
+           } 
+          self.digits[i] -= *curr_rhs_digit; 
+        }
+        self.remove_leading_zeros();
+    }
 }
 
 impl AddAssign for Int {
@@ -71,6 +109,32 @@ impl Add for Int {
         res += other;
         return res;
     }
+}
+
+impl SubAssign for Int {
+    fn sub_assign(&mut self, other: Int) {
+       if self.is_negative || other.is_negative || match less_in_magnitude(self, &other){
+            Ordering::Less => true,
+            Ordering::Equal => false,
+            Ordering::Greater => false,
+        } {
+        unimplemented!();
+       } 
+       self.subtract_ignoring_sign(&other);
+    }
+}
+
+impl Sub for Int {
+    type Output = Int;
+
+    fn sub(self, other: Int) -> Int {
+       let mut res = Int {
+            is_negative: self.is_negative,
+            digits: self.digits,
+        };
+        res -= other;
+        return res;
+    }   
 }
 
 impl PartialOrd for Int {
@@ -258,6 +322,36 @@ mod tests {
             Int {
                 is_negative: false,
                 digits: vec![2147483645, 1],
+            }
+        );
+    }
+
+    #[test]
+    fn sub_test() {
+        let a = Int::new_from_i32(0) - Int::new_from_i32(0);
+        assert_eq!(
+            a,
+            Int {
+                is_negative: false,
+                digits: vec![0],
+            }
+        );
+        let b = Int::new_from_i32(3) - Int::new_from_i32(2);
+        assert_eq!(
+            b,
+            Int {
+                is_negative: false,
+                digits: vec![1],
+            }
+        );
+        let mut c = Int::new_from_i32(i32::max_value()) + Int::new_from_i32(i32::max_value())
+            + Int::new_from_i32(i32::max_value());
+            c-= Int::new_from_i32(1);
+        assert_eq!(
+            c,
+            Int {
+                is_negative: false,
+                digits: vec![2147483644, 1],
             }
         );
     }
