ea868f17a4b3c01d3ac2d33ade253586cdda5713
Joel Laity <joel.laity@gmail.com>
2018-09-23 12:07:41 +1200

    Add benchmarks

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,24 @@
+#![feature(test)]
+
+extern crate test;
+
 pub mod integer;
 pub mod uint;
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use test::Bencher;
+
+    #[bench]
+    fn bench_add_two(b: &mut Bencher) {
+        b.iter(|| {
+            let mut a = uint::UInt::from(450);
+            a *= &a.clone();
+        });
+    }
+}
